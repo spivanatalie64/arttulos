dd06f5df8c58fa48040cc87d4a53cbb7986c6903
Natalie Spiva <spivajohnathan64@gmail.com>
2026-05-25 15:51:56 -0700

    Update post-install to use install-all.sh

--- configs/post-install.sh
+++ configs/post-install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # Run all post-install integration scripts for ArttulOS
 set -e
-
-bash /root/pacnixum-install.sh
-bash /root/ai-assistant-install.sh
-bash /root/branding-install.sh
-bash /root/scrapec-install.sh
+bash /root/configs/install-all.sh
